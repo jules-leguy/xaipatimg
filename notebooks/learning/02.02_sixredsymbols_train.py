9309f21aa5a74257d5216351ac05335f77952bfe
Sun-Setthika <sunsetthika@gmail.com>
2025-06-20 14:38:38 +0200

    train 6 red symbols, 6 squares, twice as many green as red

---
+++ notebooks/learning/02.02_sixredsymbols_train.py
@@ -0,0 +1,19 @@
+import os
+
+os.environ["DATA"] = os.path.expanduser("~/")
+
+db_dir = os.environ["DATA"] + "PatImgXAI_data/db0.1.4_6x6/"
+train_dataset_filename = "exactly_6_red_symbols_train.csv"
+test_dataset_filename = "exactly_6_red_symbols_test.csv"
+valid_dataset_filename = "exactly_6_red_symbols_valid.csv"
+model_dir = os.environ["DATA"] + "models/db0.1.4_6x6/exactly_6_red_symbols_80accuracy_model/"
+
+device = "cuda:0"
+
+from xaipatimg.ml.learning import train_resnet18_model
+
+train_resnet18_model(db_dir, train_dataset_filename, valid_dataset_filename, model_dir, device=device)
+
+from xaipatimg.ml.learning import compute_resnet18_model_scores
+
+compute_resnet18_model_scores(db_dir, train_dataset_filename, test_dataset_filename, valid_dataset_filename, model_dir, device=device)
