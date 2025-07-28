6b1f03c4722ef862f1efaee9b071e6a8086f055c
Sun-Setthika <sunsetthika@gmail.com>
2025-07-28 16:38:59 +0200

    error import for load db

--- notebooks/datagen/datagen_v0.1.5.py
+++ notebooks/datagen/datagen_v0.1.5.py
@@ -33,8 +33,29 @@ To generate the images, the user must specify their content represented in a str
 """###Load and Save Database
 """
 from xaipatimg.datagen.dbimg import load_db
+def load_db(db_dir):
+    """
+    Load the database from the given path. If the file does not exist, returns an empty DB.
+    the purpose of load db is to access the db.json
+    :param db_dir: path to the root directory of the database
+    :return: database object
+    """
+    try:
+        with open(os.path.join(db_dir, "db.json")) as json_file:
+            json_data = json.load(json_file)
+            return json_data
+    except FileNotFoundError:
+        return {}
+def save_db(db_dir, db):
+    """
+    Save the given database to the given path
+    :param db_dir: path to the root directory of the database
+    :param db: database object
+    :return: None
+    """
+    with open(os.path.join(db_dir, "db.json"), 'w', encoding='utf-8') as f:
+        json.dump(db, f, ensure_ascii=False, indent=4)
 db = load_db(db_dir)
-
 print(db)
 
 """Define number of image generation and grid"""
